896ca0a19ed1fb4a619f4d21bf4fd1d5311cf85a
Ahraman <ahraman12000@gmail.com>
2024-07-02 03:41:37 +0330

    created foundation for migrations and entities

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,4 +1,8 @@
+use sea_orm::{ConnectOptions, Database, DbErr};
 use sea_orm_migration::{MigrationTrait, MigratorTrait};
+
+mod v0_1_0;
+use v0_1_0::*;
 
 pub struct Migrator;
 
@@ -6,4 +10,42 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![]
     }
+}
+
+#[derive(Debug, Clone)]
+pub struct DatabaseAddress {
+    pub backend: String,
+    pub username: String,
+    pub password: String,
+    pub url: String,
+    pub name: String,
+}
+
+impl DatabaseAddress {
+    pub fn from_env() -> Result<Self, std::env::VarError> {
+        Ok(Self {
+            backend: std::env::var("DB_BACKEND")?,
+            username: std::env::var("DB_USER")?,
+            password: std::env::var("DB_PASS")?,
+            url: std::env::var("DB_URL")?,
+            name: std::env::var("DB_NAME")?,
+        })
+    }
+
+    pub fn to_string(&self) -> String {
+        format!(
+            "{}://{}:{}@{}/{}",
+            self.backend, self.username, self.password, self.url, self.name
+        )
+    }
+}
+
+pub async fn run(address: DatabaseAddress) -> Result<(), DbErr> {
+    let database_url = address.to_string();
+    let options = ConnectOptions::new(&database_url);
+
+    let conn = Database::connect(options).await?;
+    Migrator::up(&conn, None).await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use axum::{routing::get, Router};
+use euclidius_migration::DatabaseAddress;
+use sea_orm::DbErr;
 use tokio::net::TcpListener;
 
 use euclidius::routing::{page, root};
@@ -7,10 +9,19 @@ use euclidius::routing::{page, root};
 async fn main() {
     dotenvy::dotenv().expect("File not found");
 
+    init_database_connection().await.unwrap();
+
     let listener = TcpListener::bind("localhost:3000").await.unwrap();
 
     let router = Router::new()
         .route("/", get(root::show))
         .route("/:title", get(page::show));
     axum::serve(listener, router).await.unwrap();
+}
+
+async fn init_database_connection() -> Result<(), DbErr> {
+    let address = DatabaseAddress::from_env().unwrap();
+    euclidius_migration::run(address).await?;
+
+    Ok(())
 }
